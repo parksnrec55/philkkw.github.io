f0159e03faf4ca3b5db6e5f65a6491b4f84c6305
pgoldy5 <gol13002@byui.edu>
2017-04-04 20:44:57 -0600

    acme update

--- acme/js/acme-script.js
+++ acme/js/acme-script.js
@@ -1,24 +1,35 @@
 $(function() {
-    $("#nav-items").append('<li><a href="link">Home</a></li>');
+    $("#nav-items").append('<li id="home"><a href="link">Home</a></li>');
 
 
   $.getJSON('js/acme.json',function(result) {
       $.each(result.navigation, function(i, nav){
           console.log(result);
-          var name = result.navigation[i].name;
+          var name = result.navigation[i].nameshort;
           console.log(name);
 
-          $("#nav-items").append('<li id="#' + name.toLowerCase + '"><a href="link">' + name + '</a></li>');
+          $("#nav-items").append('<li id="' + name.toLowerCase() + '"><a href="link">' + name + '</a></li>');
+          console.log(name.toLowerCase());
+          console.log("<li id=\"" + name.toLowerCase() + "\"><a href=\"#\">" + name + "</a></li>")
       })
       console.log("Success");
   }).fail(function() {
       console.log("Error");
   });
+    
+    $('#home').click( function( event ){
+        event.preventDefault();
+        $('#homecontent').hide();
+      })
+    $("#anvil").click( function ( event ){
+        event.preventDefault();
+        $("#homecontent").hide();
+    })
+
 
 
 
 
 
   document.title = 'JS IS WORKING';
-  $('#nav-items').append("<li><a href=\"link\">LINKER</a></li>")
 })
